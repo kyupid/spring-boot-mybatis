f67d19662f2ca899de88e4b7eef2a0c990ec6e91
kyupid <iamscvv@gmail.com>
2021-12-24 20:40:45 +0900

    요청 받을시 객체 매핑을 위한 getter

--- src/main/java/com/example/demo/Employee.java
+++ src/main/java/com/example/demo/Employee.java
@@ -3,4 +3,20 @@ package com.example.demo;
 public class Employee {
     private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
