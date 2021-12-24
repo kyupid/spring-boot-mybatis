ad8e3b498a043b11ff993832c3898b64979b3651
kyupid <iamscvv@gmail.com>
2021-12-24 20:45:22 +0900

    getter 대신 롬복 `@Data` 사용

--- src/main/java/com/example/demo/Employee.java
+++ src/main/java/com/example/demo/Employee.java
@@ -1,16 +1,11 @@
 package com.example.demo;
 
+import lombok.Data;
+
+@Data
 public class Employee {
     private int id;
     private String name;
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
 
     @Override
     public String toString() {
