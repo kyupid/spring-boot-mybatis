7b45a391a4a78d8231414d9e58c4e369a22146c4
kyupid <iamscvv@gmail.com>
2021-12-24 19:21:55 +0900

    컨트롤러에서 서비스 인터페이스가 아니라 구현체를 `@Autowird` 함

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,10 +8,10 @@ import java.util.List;
 
 @Controller
 public class EmployeeController {
-    private EmployeeService employeeService;
+    private EmployeeServiceImplement employeeService;
 
     @Autowired
-    public EmployeeController(EmployeeService employeeService) {
+    public EmployeeController(EmployeeServiceImplement employeeService) {
         this.employeeService = employeeService;
     }
 
